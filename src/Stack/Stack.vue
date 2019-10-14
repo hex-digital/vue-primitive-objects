c1b70b8d44d5b12fe5b2fa8c8edfcaa8a9b2454b
Jamie Warburton <jamie@hexdigital.com>
2019-10-14 12:31:46 +0100

    Add first pass at Stack primitive

---
+++ src/Stack/Stack.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="stackClasses" :data-identifier="configIdentifier">
+    <slot />
+  </div>
+</template>
+
+<script>
+/**
+ * Flow elements require space to physically and conceptually separate them from the elements that come before and
+ * after them. This flow space should exist solely between children elements in a container, and not between an element
+ * and the container itself.
+ * See https://absolutely.every-layout.dev/layouts/stack/
+ *
+ * Sometimes content should split, with some aligned to the top of the container, and some aligned to the bottom.
+ * This can be achieved using the splitAfter prop, which will anchor all children after the nth to the bottom.
+ * Useful for e.g. pinning buttons or text to the bottom of the container.
+ *
+ * Stacks can be used within stacks as well. If recursive is true, all stacks within the parent will have the same
+ * sizing. If recursive is false, all nested stacks can designate their own spacing value.
+ * This could be useful for laying out a form, where elements are grouped into a stack, and labels + inputs exist inside
+ * of nested stacks with different spacing.
+ */
+export default {
+  name: 'Stack',
+  props: {
+    recursive: { type: Boolean, default: false },
+    space: { type: String, default: '1.5rem' }, // @todo Prob a vertical rhythm value here
+    splitAfter: {
+      type: Number,
+      default: 0,
+      validator: (value) => value >= 0 && Number.isInteger(value),
+    },
+  },
+  computed: {
+    configIdentifier() {
+      return `stack-${this.splitAfter}`;
+    },
+    stackClasses() {
+      return {
+        'o-stack': true,
+        'o-stack--recursive': this.recursive,
+      };
+    },
+  },
+  beforeMount() {
+    if (this.splitAfter === 0) return;
+    if (document.getElementById(this.configIdentifier)) return;
+    document.head.innerHTML += `
+      <style id="${this.configIdentifier}">
+        ${`[data-identifier="${this.configIdentifier}"]:only-child {
+            height: 100%;
+          }
+          [data-identifier="${this.configIdentifier}"] > :nth-child(${this.splitAfter}) {
+            margin-bottom: auto;
+          }`}
+      </style>
+      `
+      .replace(/\s\s+/g, ' ')
+      .trim();
+  },
+};
+</script>
+
+<style lang="scss">
+.o-stack {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.o-stack > * + *,
+.o-stack--recursive * + * {
+  margin-top: 1.5rem; // @todo This value should come from the modular spacing scale
+}
+</style>
