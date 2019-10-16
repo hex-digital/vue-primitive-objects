65b946018ca5e4c4267fcc73a75805f58bb75f27
Jamie Warburton <jamie@hexdigital.com>
2019-10-16 09:50:34 +0100

    Make the Switcher props work

--- src/components/Switcher/Switcher.vue
+++ src/components/Switcher/Switcher.vue
@@ -1,29 +1,60 @@
 <template>
-  <div class="o-switcher">
+  <div class="o-switcher" :data-identifier="configIdentifier">
     <slot />
   </div>
 </template>
 
 <script>
+import { stylesFromComponentConfig } from '../../mixins/stylesFromComponentConfig';
+
 export default {
   name: 'Switcher',
+  mixins: [stylesFromComponentConfig],
   props: {
-    limit: { type: Number, default: 4 }, // @todo This needs to be implemented
-    space: { type: String, default: '1.5rem' }, // @todo This needs to be implemented
-    threshold: { type: String, default: '60ch' }, // @todo This needs to be implemented
+    limit: { type: Number, default: 4 },
+    space: { type: String, default: '1.5rem' }, // @todo This should come from the modular sizings
+    threshold: { type: String, default: '60ch' },
   },
   computed: {
+    configVariables() {
+      return `${this.threshold}${this.adjustedSpace}${this.limit}`;
+    },
     adjustedSpace() {
       return this.space === '0' ? '0px' : this.space;
+    },
+  },
+  methods: {
+    configStyles(selector) {
+      // If threshold is 60ch, and the (width - adjustedSpace) is 50ch, sizeFromThreshold is 10.
+      // If (width - adjustedSpace) was 70ch, sizeFromThreshold is -10.
+      // @todo Improve the name of this variable to better describe it
+      const sizeFromThreshold = `(${this.threshold} - (100% - ${this.adjustedSpace}))`;
+      const limit = parseInt(this.limit);
+
+      return `
+        ${selector} > * {
+          margin: calc((${this.adjustedSpace} / 2) * -1);
+        }
+
+        ${selector} > * > * {
+          flex-basis: calc(${sizeFromThreshold} * 999);
+          margin: calc(${this.adjustedSpace} / 2);
+        }
+
+        ${selector} > * > :nth-last-child(n+${limit + 1}),
+        ${selector} > * > :nth-last-child(n+${limit + 1}) ~ * {
+          flex-basis: 100%;
+        }
+      `;
     },
   },
 };
 </script>
 
 <style lang="scss">
-$measure: 60ch; // @todo
-$space: 1.5rem; // @todo This should come from the modular sizings
-$threshold: 4; // @todo
+$defaultMeasure: 60ch;
+$defaultSpace: 1.5rem; // @todo This should come from the modular sizings
+$defaultThreshold: 4;
 
 .o-switcher {
   display: block;
@@ -32,20 +63,13 @@ $threshold: 4; // @todo
 .o-switcher > * {
   display: flex;
   flex-wrap: wrap;
-  margin: calc((#{$space} / 2) * -1);
+  margin: calc((#{$defaultSpace} / 2) * -1);
   overflow: hidden;
 }
 
 .o-switcher > * > * {
-  flex-basis: calc((#{$measure} - 100%) * 999);
+  flex-basis: calc((#{$defaultThreshold} - (100% - #{$defaultSpace})) * 999);
   flex-grow: 1;
-  // @todo Implement threshold: flex-basis: calc((#{$threshold} - (100% - #{$space})) * 999);
-  margin: calc(#{$space} / 2);
+  margin: calc(#{$defaultSpace} / 2);
 }
-
-// @todo This may need to be an injected stylesheet instead of the below
-//.o-switcher > * > :nth-last-child(n+${parseInt(this.limit) + 1}),
-//.o-switcher > * > :nth-last-child(n+${parseInt(this.limit) + 1}) ~ * {
-//  flex-basis: 100%;
-//}
 </style>
